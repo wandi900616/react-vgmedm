1b2b6c0801a25fb717572a45dcdb43fdc3ffd30a
wandi900616 <ease.ja@gmail.com>
2020-12-26 12:07:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-vgmedm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-vgmedm)
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjTYY-zrOjy08DaKIk2QJ4bqoGufLNKcM",
+  authDomain: "myrph-63082.firebaseapp.com",
+  projectId: "myrph-63082",
+  storageBucket: "myrph-63082.appspot.com",
+  messagingSenderId: "649243202454",
+  appId: "1:649243202454:web:022f6ff488e03ab66743b7",
+  measurementId: "G-FP9N6TCXGP"
+};
